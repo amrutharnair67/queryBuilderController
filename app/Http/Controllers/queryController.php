31531b326856e1d2e2fbd1278cc8e8afb6387975
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-08 15:24:51 +0530

    commiyt

--- app/Http/Controllers/queryController.php
+++ app/Http/Controllers/queryController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class queryController extends Controller
 {
     public function index(Request $request){
-        $data=DB::table('studentmark')->where('age','>',21)->count();
+        $data=DB::table('studentmark')->where('age','>',21)->exists();
         // where('age','>',21)->value('id');
         dd($data);
 //             [
