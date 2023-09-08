90299b1447318411f271d3659ea5b88f0abea26e
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-08 15:22:19 +0530

    first commit

---
+++ app/Http/Controllers/queryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class queryController extends Controller
+{
+    public function index(Request $request){
+        $data=DB::table('studentmark')->avg('age');
+        // where('age','>',21)->where('age','>',21)->value('id');
+        dd($data);
+//             [
+//                 "name"=>"amMU",
+//                 "age"=>25,
+                
+//             ]
+            
+//             );
+     } }
